5de24bd0aa831dcbb05d636f905420989c3d9d6a
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-27 20:20:58 +0000

    Sat, Jan 27, 2024, 8:20 PM +00:00

---
+++ Week04/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace Week04;
+
+class Person
+{
+    public string Name { get; }
+    public int Age { get; }
+    public double Weight { get; }
+    public bool IsMarried { get; }
+
+    public Person(string name, int age, double weight,
+      bool isMarried = true)
+    {
+        Name = name; Age = age; Weight = weight;
+        IsMarried = isMarried;
+    }
+}
--- Week04/Program.cs
+++ Week04/Program.cs
@@ -1,32 +1,69 @@
-﻿String filename = "output.txt";
-// TextWriter writer = new StreamWriter(filename);  //Declare and initialise 
-//                                                  //the object to write to
+﻿namespace Week04;
 
-// writer.Write("Narendra");                      //Do some writing
-// writer.WriteLine("Pershad");
-// writer.WriteLine("================");
-
-// for (int i = 1; i <= 12; i++)
-// {
-//     writer.WriteLine($"{i,2} x 15 = {i * 15}");     //Do more writing
-// }
-
-// writer.Close();                                  //Now close the stream object
-
-
-TextReader reader = new StreamReader(filename); //Initialise the reader object
-
-string line = reader.ReadLine();                //Do some reading
-Console.WriteLine(line);                        //Display what was read
-
-line = reader.ReadLine();                       //Do some reading
-
-while (line != null)                             //Stop if you didn’t read anything
+class Program
 {
-    Console.WriteLine(line);                      //Display what was read
-    line = reader.ReadLine();                     //Do more reading
+    public static void Main(string[] args)
+    {
+        String filename = "output.txt";
+        // TextWriter writer = new StreamWriter(filename);  //Declare and initialise 
+        //                                                  //the object to write to
+
+        // writer.Write("Narendra");                      //Do some writing
+        // writer.WriteLine("Pershad");
+        // writer.WriteLine("================");
+
+        // for (int i = 1; i <= 12; i++)
+        // {
+        //     writer.WriteLine($"{i,2} x 15 = {i * 15}");     //Do more writing
+        // }
+
+        // writer.Close();                                  //Now close the stream object
+
+
+        // TextReader reader = new StreamReader(filename); //Initialise the reader object
+
+        // string line = reader.ReadLine();                //Do some reading
+        // Console.WriteLine(line);                        //Display what was read
+
+        // line = reader.ReadLine();                       //Do some reading
+
+        // while (line != null)                             //Stop if you didn’t read anything
+        // {
+        //     Console.WriteLine(line);                      //Display what was read
+        //     line = reader.ReadLine();                     //Do more reading
+        // }
+
+        // reader.Close();                                 //Now close the stream object
+
+        // File.ReadAllText(filename);
+
+        TextReader reader = new StreamReader(filename);  //Declare and initialise a text reader
+        List<Person> persons = new List<Person>();      //list to store the objects                                          
+        string line = reader.ReadLine();                 //read the first line
+        while (line != null)                              //read the first line
+        {
+            string[] values = line.Split('\t');
+            if (values.Length >= 4)
+            {
+                string name = values[0];
+                double weight = Convert.ToDouble(values[1]);
+                int age = Convert.ToInt32(values[2]);
+                bool isMarried = Convert.ToBoolean(values[3]);
+                persons.Add(new Person(name, age, weight, isMarried)); //add object to the list
+            }
+            else
+            {
+                Console.WriteLine($"Line '{line}' does not contain the expected number of elements.");
+            }
+            string name = values[0];
+            double weight = Convert.ToDouble(values[1]);
+            int age = Convert.ToInt32(values[2]);
+            bool isMarried = Convert.ToBoolean(values[3]);
+            persons.Add(new Person(name, age, weight, isMarried)); //add object to the list
+            line = reader.ReadLine();                      //read the next line
+        }
+
+        reader.Close();                                  //Now close the stream object
+
+    }
 }
-
-reader.Close();                                 //Now close the stream object
-
-File.ReadAllText(filename);
