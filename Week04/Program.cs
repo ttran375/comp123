15a00beeeaaa1236d83098fddf97aa11ff311a86
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-27 20:03:11 +0000

    Sat, Jan 27, 2024, 8:03 PM +00:00

--- Week04/Program.cs
+++ Week04/Program.cs
@@ -1,14 +1,30 @@
 ﻿String filename = "output.txt";
-TextWriter writer = new StreamWriter(filename);  //Declare and initialise 
-                                                 //the object to write to
+// TextWriter writer = new StreamWriter(filename);  //Declare and initialise 
+//                                                  //the object to write to
 
-writer.Write("Narendra");                      //Do some writing
-writer.WriteLine("Pershad");
-writer.WriteLine("================");
+// writer.Write("Narendra");                      //Do some writing
+// writer.WriteLine("Pershad");
+// writer.WriteLine("================");
 
-for (int i = 1; i <= 12; i++)
+// for (int i = 1; i <= 12; i++)
+// {
+//     writer.WriteLine($"{i,2} x 15 = {i * 15}");     //Do more writing
+// }
+
+// writer.Close();                                  //Now close the stream object
+
+
+TextReader reader = new StreamReader(filename); //Initialise the reader object
+
+string line = reader.ReadLine();                //Do some reading
+Console.WriteLine(line);                        //Display what was read
+
+line = reader.ReadLine();                       //Do some reading
+
+while (line != null)                             //Stop if you didn’t read anything
 {
-    writer.WriteLine($"{i,2} x 15 = {i * 15}");     //Do more writing
+    Console.WriteLine(line);                      //Display what was read
+    line = reader.ReadLine();                     //Do more reading
 }
 
-writer.Close();                                  //Now close the stream object
+reader.Close();                                 //Now close the stream object
