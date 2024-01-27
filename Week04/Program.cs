87443637414426a684127be62b6ae4367f543cc5
T. Tran <ttran375@my.centennialcollege.ca>
2024-01-27 19:49:32 +0000

    Sat, Jan 27, 2024, 7:49 PM +00:00

---
+++ Week04/Program.cs
@@ -0,0 +1,14 @@
+﻿String filename = "output.txt";
+TextWriter writer = new StreamWriter(filename);  //Declare and initialise 
+                                                 //the object to write to
+
+writer.Write("Narendra");                      //Do some writing
+writer.WriteLine("Pershad");
+writer.WriteLine("================");
+
+for (int i = 1; i <= 12; i++)
+{
+    writer.WriteLine($"{i,2} x 15 = {i * 15}");     //Do more writing
+}
+
+writer.Close();                                  //Now close the stream object
