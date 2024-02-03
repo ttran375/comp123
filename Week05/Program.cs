b349e1017611db3773e45363c43ff41936ac9a06
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-03 19:49:11 +0000

    Sat, Feb 3, 2024, 7:49 PM +00:00

---
+++ Week05/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+// Define the object you want to serialize here. For example, a simple class instance.
+var objectToSerialize = new MyClass();
+
+BinaryFormatter formatter = new BinaryFormatter();
+using (Stream stream = new FileStream("MyFile.bin", FileMode.Create, FileAccess.Write, FileShare.None))
+{
+    formatter.Serialize(stream, objectToSerialize);
+}
