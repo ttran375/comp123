1257b7e5d60b508b9e61c5e6797bd7db7eee049e
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-10 19:54:48 +0000

    Sat, Feb 10, 2024, 7:54 PM +00:00

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class ClassA
+{
+    public ClassA() { Console.WriteLine("ClassA"); }
+}
+
+class ClassB : ClassA
+{
+    public ClassB() { Console.WriteLine("ClassB"); }
+}
+
+class ClassC : ClassB
+{
+    public ClassC() { Console.WriteLine("ClassC"); }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassC obj = new ClassC();
+    }
+}
