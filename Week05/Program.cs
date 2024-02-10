13a135383fe67ac583decabf8074f91ef6f15c56
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-10 20:05:19 +0000

    Sat, Feb 10, 2024, 8:05 PM +00:00

---
+++ Week05/Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+        // Creating an instance of the Child class will invoke the Parent and Child constructors
+        Child childObject = new Child();
+
+        // Rest of the program logic, if any
+    }
+}
+
+class Parent
+{
+    public Parent(string name)
+    {
+        // Constructor logic here
+        Console.WriteLine($"Parent constructor called with name: {name}");
+    }
+}
+
+class Child : Parent
+{
+    public Child() : base("Narendra")
+    {
+        // Child-specific constructor logic here
+        Console.WriteLine("Child constructor called");
+    }
+}
