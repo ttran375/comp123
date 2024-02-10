bea3eb5807e2e78153ab5adf96242e35e64f7e32
T. Tran <ttran375@my.centennialcollege.ca>
2024-02-10 20:57:05 +0000

    Sat, Feb 10, 2024, 8:57 PM +00:00

--- Week05/Program.cs
+++ Week05/Program.cs
@@ -1,24 +1,107 @@
-﻿class AParent
+﻿class Program
 {
-    public virtual void HiddenMethod()
+    static void Main(string[] args)
     {
-        Console.WriteLine("In Parent");
+        Address address = new Address("123", "Main St", "Anytown", "Anystate", "AnyCountry", "AnyProvince", "12345");
+        Console.WriteLine(address.ToString());
     }
 }
 
-class AChild : AParent
+public class Address
 {
-    public void HiddenMethod()
-    {
-        Console.WriteLine("In Child");
-    }
-}
+    string StreetNumber;
+    string StreetName;
+    string City;
+    string State;
+    string Country;
+    string StateProvince;
+    string StateZipCode;
 
-class Program
-{
-    static void Main()
+    public string getStreetNumber()
     {
-        AParent parent = new AChild();
-        parent.HiddenMethod(); // Output: In Parent
+        return this.StreetNumber;
+    }
+
+    public void setStreetNumber(string StreetNumber)
+    {
+        this.StreetNumber = StreetNumber;
+    }
+
+    public string getStreetName()
+    {
+        return this.StreetName;
+    }
+
+    public void setStreetName(string StreetName)
+    {
+        this.StreetName = StreetName;
+    }
+
+    public string getCity()
+    {
+        return this.City;
+    }
+
+    public void setCity(string City)
+    {
+        this.City = City;
+    }
+
+    public string getState()
+    {
+        return this.State;
+    }
+
+    public void setState(string State)
+    {
+        this.State = State;
+    }
+
+    public string getCountry()
+    {
+        return this.Country;
+    }
+
+    public void setCountry(string Country)
+    {
+        this.Country = Country;
+    }
+
+    public string getStateProvince()
+    {
+        return this.StateProvince;
+    }
+
+    public void setStateProvince(string StateProvince)
+    {
+        this.StateProvince = StateProvince;
+    }
+
+    public string getStateZipCode()
+    {
+        return this.StateZipCode;
+    }
+
+    public void setStateZipCode(string StateZipCode)
+    {
+        this.StateZipCode = StateZipCode;
+    }
+
+
+    public Address(string streetNumber, string streetName, string city, string state, string country, string stateProvince, string stateZipCode)
+    {
+        StreetNumber = streetNumber;
+        StreetName = streetName;
+        City = city;
+        State = state;
+        Country = country;
+        StateProvince = stateProvince;
+        StateZipCode = stateZipCode;
+    }
+
+
+    public override string ToString()
+    {
+        return $"{StreetNumber} {StreetName}, {City}, {State}, {Country}, {StateProvince}, {StateZipCode}";
     }
 }
