86d16fe30534e3ed4a636851a8017f80e53d529a
Dat Tran <datttrian@gmail.com>
2024-02-23 16:51:19 +0000

    Update README.md

--- Week03/PartialNested/Program.cs
+++ Week03/PartialNested/Program.cs
@@ -1,2 +1,65 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Define Foo in partial class files
+// in foo_b.cs 
+public partial class Foo
+{
+    public Foo()
+    {
+    }
+    public void Bar()
+    {
+    }
+}
+
+// in foo_a.cs
+public partial class Foo
+{
+    public void Baz()
+    {
+    }
+    public override string ToString()
+    {
+        return "Custom ToString() method";
+    }
+}
+
+// Define Container class
+public class Container
+{
+    public int Foo;
+    public void Bar()
+    {
+    }
+    public class Nested
+    {
+        public void Baz()
+        {
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Anonymous type usage
+        var a = new { Name = "Trudeau", Age = 25 };
+        Console.WriteLine(a); // Output: { Name = Trudeau, Age = 25 }
+
+        // Instantiate Foo and use its methods
+        Foo foo = new Foo();
+        foo.Bar();
+        foo.Baz();
+        Console.WriteLine(foo.ToString()); // Output: Custom ToString() method
+
+        // Instantiate Container and use its members
+        Container container = new Container();
+        container.Foo = 10;
+        container.Bar();
+
+        // Instantiate Nested class and use its methods
+        Container.Nested nested = new Container.Nested();
+        nested.Baz();
+    }
+}
