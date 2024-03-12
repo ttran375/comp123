adfcdf5152e72340a34b85cb451a6fe607af6af2
T. Tran <ttran375@my.centennialcollege.ca>
2024-03-12 09:13:09 +0000

    Update solutions

--- Types/Program.cs
+++ Types/Program.cs
@@ -11,14 +11,18 @@ class Program
     static void Main(string[] args)
     {
         Rectangle bigRect;              //declare the variable
-        bigRect = new Rectangle();      //allocate storage
-        bigRect.length = 8;             //sets the length to 8
-        bigRect.width = 5;              //sets the width to 5
+        bigRect = new Rectangle
+        {
+            length = 8,             //sets the length to 8
+            width = 5              //sets the width to 5
+        };      //allocate storage
 
         //declaration and allocation one step 
-        Rectangle smallRect = new Rectangle();
-        smallRect.length = 3;           //sets the length to 3
-        smallRect.width = 2;            //sets the width to 2
+        Rectangle smallRect = new Rectangle
+        {
+            length = 3,           //sets the length to 3
+            width = 2            //sets the width to 2
+        };
 
         DescribeRectangle(bigRect);
         DescribeRectangle(smallRect);
@@ -44,9 +48,11 @@ class Program
 
     public static Rectangle CreateRectangle(int width, int length)
     {
-        Rectangle result = new Rectangle();
-        result.width = width;
-        result.length = length;
+        Rectangle result = new Rectangle
+        {
+            width = width,
+            length = length
+        };
         return result;
     }
 }
